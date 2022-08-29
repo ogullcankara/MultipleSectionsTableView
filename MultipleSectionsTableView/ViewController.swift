8c4012f23f068cb78cc916cb2438eab007e24760
ogullcankara <ogullcankara@gmail.com>
2022-08-29 14:46:40 +0300

    First Commit

--- MultipleSectionsTableView/ViewController.swift
+++ MultipleSectionsTableView/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var bolumler = ["Meyveler","Renkler","Mevsimler"]
+    
+    var bolumverileri = [["Elma","Muz","Armut","Karpuz"],["Kırmızı","Mavi","Yeşil"],["Yaz","Kış"]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
 
 
+}
+
+extension ViewController:UITableViewDelegate,UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return bolumler.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bolumverileri[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return bolumler[section]
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "hucreadi", for: indexPath)
+        
+        cell.textLabel?.text = bolumverileri[indexPath.section][indexPath.row]
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(bolumler[indexPath.section]) bölümünden \(bolumverileri[indexPath.section][indexPath.row]) secildi")
+    }
+    
 }
 
